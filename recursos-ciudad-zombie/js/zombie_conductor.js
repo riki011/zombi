f53c1042a5bcbd6ffbaa51bfdd17e99d811af3b7
riki011 <54559308+riki011@users.noreply.github.com>
2020-03-02 16:05:26 -0300

    Add files via upload

---
+++ recursos-ciudad-zombie/js/zombie_conductor.js
@@ -0,0 +1,46 @@
+/* Para insipirarte para la implementacion del ZombieConductor podes usar
+al ZombieCaminante de ejemplo. Tene en cuenta que tendra algunas diferencias.
+Por ejemplo, la cantidad parametros que recibe su constructor. En ZombieConductor
+no son exactamente los mismos parametros que en el objeto Enemigo, a diferencia
+del ZombieCaminante que eran los mismos. */
+
+var ZombieConductor = function(sprite, x, y, ancho, alto, velocidad, rangoMov={desdeX:valor,hastaX:valor,desdeY:valor,hastaY:valor},direccion) {
+  /* Completar constructor a partir de Enemigo */
+  //Enemigo.call(/* ... */);
+  /* No olvidar agregar la/s propiedad/es unicas de ZombieConductor necesarias */
+  Enemigo.call(this, sprite, x, y, ancho, alto, velocidad, rangoMov,direccion);
+}
+
+/* Completar creacion del ZombieConductor */
+ZombieConductor.prototype = Object.create(Enemigo.prototype);
+ZombieConductor.prototype.constructor = ZombieConductor;
+
+/* Completar metodos para el movimiento y el ataque */
+
+ZombieConductor.prototype.mover = function() {
+  /* Los movimientos estan basados en un numero aleatorio
+  La direccion horizontal es siempre la misma y va ondulando verticalmente.
+  Esto hasta llegar a sus limites, donde se invierte su direccion horizontal */
+ // console.log('holaconductor');
+  if (Math.random() < 0.5) {
+    this.x -= this.velocidad;
+    this.y -= this.velocidad;
+  } else {
+    //Sino, hace otro movimiento
+    this.y += this.velocidad;
+    this.x -= this.velocidad;
+  }
+
+  /* En esta parte lo que hacemos es invertir la direccion horizontal si
+  toca uno de sus limites, modificando su velocidad. Si multiplicamos por -1 la
+  velocidad lo que estamos haciendo es invertir su direccion.*/
+  if ((this.x < this.rangoMov.desdeX) || (this.x > this.rangoMov.hastaX)){
+    this.velocidad *= -1;
+      console.log('holaconductor');
+  }
+  // Si sobrepasa el rangoY, lo manda al centro entre ambos rangos
+  if ((this.y < this.rangoMov.desdeY) || (this.y > this.rangoMov.hastaY)) {
+    this.y = this.rangoMov.desdeY + (this.rangoMov.hastaY - this.rangoMov.desdeY)/2;
+      console.log('holaconductor');
+  }
+}
